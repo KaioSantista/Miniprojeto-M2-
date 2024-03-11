ae5d445cd35e7748901b376d622648184eff8fb4
Kaio Augusto Pereira dos Santos <akaio0202@gmail.com>
2024-03-11 07:16:35 -0300

    Tela frond end

---
+++ script.js
@@ -0,0 +1,17 @@
+class Jogos{
+    constructor()
+    {
+        this.id=0;
+        this.nomeJogo='';
+        this.generoJogo='';
+        this.valorjogo=0;
+    }
+    adicionar(){
+
+    }
+    excluir(){
+
+    }
+    
+}
+let jogos= new Jogos
